84bd956bb75c82e2d290a7cf6f8f7c57b8dd943b
isabel k. lee <hello@isabelklee.co>
2020-04-06 17:23:56 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: "How to Bake Bread", description: "Baking Bread is very good.")
+Article.create(title: "5 ways to make chocolate chip cookie dough", description: "Baking rules.")
